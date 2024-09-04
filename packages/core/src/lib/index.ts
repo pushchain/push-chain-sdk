05f157c434a668cf500a3cebe4cb000007b49cb9
aman035 <guptaaman200115@gmail.com>
2024-09-04 18:02:18 +0530

    fix: exports

--- packages/core/src/lib/index.ts
+++ packages/core/src/lib/index.ts
@@ -1,4 +1,14 @@
 import { PushNetwork } from './pushNetwork';
+import { Block } from './block/block';
+import { Tx } from './tx/tx';
+import { Validator } from './validator/validator';
 import { CONSTANTS } from './constants';
 
-export { PushNetwork, CONSTANTS };
+/**
+ * FOR ADVANCE USECASES
+ * serialization / deserialization capabilities for block & tx
+ * Validator calls
+ */
+export { CONSTANTS, Block, Tx, Validator };
+
+export default PushNetwork;
