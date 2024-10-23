d1f7c65739ba2aaa593a0dd34c6a9274c9aff3e3
strykerin <dacosta.pereirafabio@gmail.com>
2024-10-23 15:17:32 +0400

    finish login page

--- examples/poker-app/src/App.tsx
+++ examples/poker-app/src/App.tsx
@@ -1,10 +1,27 @@
+import { usePrivy } from '@privy-io/react-auth';
 import './App.css';
 import Login from './components/login';
+import { useAppContext } from './context/app-context';
+import LoggedInView from './components/logged-in-view';
 
 function App() {
+  const { ready, authenticated } = usePrivy();
+  const { pushAccount } = useAppContext();
+
   return (
     <div className="flex justify-center items-center h-screen">
-      <Login />
+      {ready ? (
+        authenticated || pushAccount ? (
+          <LoggedInView />
+        ) : (
+          <Login />
+        )
+      ) : (
+        <div className="flex flex-col gap-4 items-center justify-center">
+          <div className="w-8 h-8 animate-spin rounded-full border-t-2 border-b-2 border-blue-500"></div>
+          <p>Loading...</p>
+        </div>
+      )}
     </div>
   );
 }
---
+++ examples/poker-app/src/components/logged-in-view.tsx
@@ -0,0 +1,3 @@
+export default function LoggedInView() {
+  return <div>LoggedInView</div>;
+}
