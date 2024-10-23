4f4987cd8b89388b7dc59252d764804a9f88b730
strykerin <dacosta.pereirafabio@gmail.com>
2024-10-23 14:38:04 +0400

    configuring poker game app

---
+++ examples/poker-app/src/App.tsx
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="flex justify-center items-center h-screen bg-red-500">
+      <h1 className="text-8xl font-bold underline">Hello world!</h1>
+    </div>
+  );
+}
+
+export default App;
