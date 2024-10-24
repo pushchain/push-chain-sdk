be494f07ce576a0352344e46a01ab2855eba08cb
strykerin <dacosta.pereirafabio@gmail.com>
2024-10-24 15:51:20 +0400

    add input to place bet

--- examples/poker-app/src/components/game.tsx
+++ examples/poker-app/src/components/game.tsx
@@ -42,6 +42,17 @@ function MyHand() {
         <img className="w-36" src={cardImageURL({ suit: 'H', rank: '6' })} />
         <img className="w-36" src={cardImageURL({ suit: 'C', rank: 'K' })} />
       </div>
+      <input
+        type="number"
+        step="1"
+        min="1"
+        max="100"
+        className="border border-gray-300 rounded-lg p-3 text-center w-1/3 shadow-md focus:outline-none focus:ring-2 focus:ring-blue-500 transition duration-200 ease-in-out"
+        placeholder="Place your bet"
+      />
+      <button className="bg-blue-500 text-white px-4 py-2 rounded-lg shadow-md mt-4">
+        Bet
+      </button>
     </div>
   );
 }
