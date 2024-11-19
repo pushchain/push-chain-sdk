03838ff36d5cd1698d1cc3218665f278c906ba7e
aman035 <guptaaman200115@gmail.com>
2024-11-19 20:01:05 +0530

    fix: publish command

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -1,14 +1,1 @@
-import { PushNetwork } from './lib/pushNetwork';
-import { Block } from './lib/block/block';
-import { Tx } from './lib/tx/tx';
-import { Validator } from './lib/validator/validator';
-import { Address } from './lib/address/address';
-import { CONSTANTS } from './lib/constants';
-
-/**
- * FOR ADVANCE USECASES
- * serialization / deserialization capabilities for block & tx
- * Validator calls
- */
-export { CONSTANTS, Block, Tx, Validator, Address };
-export default PushNetwork;
+export * from './lib';
---
+++ packages/core/src/lib/index.ts
@@ -0,0 +1,13 @@
+import { PushNetwork } from './pushNetwork';
+import { Block } from './block/block';
+import { Tx } from './tx/tx';
+import { Validator } from './validator/validator';
+import { Address } from './address/address';
+import { CONSTANTS } from './constants';
+
+/**
+ * FOR ADVANCE USECASES
+ * serialization / deserialization capabilities for block & tx
+ * Validator calls
+ */
+export { CONSTANTS, Block, Tx, Validator, Address, PushNetwork };
