0df8ad0399b78718bea9bb7af18e8d77fb6d49e7
strykerin <dacosta.pereirafabio@gmail.com>
2025-02-11 09:19:03 -0300

    create quickstart example

--- examples/fetch-transactions/index.js
+++ examples/fetch-transactions/index.js
@@ -3,27 +3,28 @@ import { CONSTANTS, PushChain } from '@pushchain/devnet';
 
 // Initialize the Push Chain SDK
 const pushChain = await PushChain.initialize();
+console.log('PushChain SDK initialized successfully.');
 
 // Fetch a transaction by its hash
 const transactionsByHash = await pushChain.tx.get(
   '177482c5a504f3922875c216f71a2b236f344cfbf334f97c8f59547e1e21fb23'
 );
+console.log('Fetched transaction by hash:', transactionsByHash);
 
-console.log(transactionsByHash);
-
-// Fetch the latest 20 transactions - all transactions
+// Fetch the latest 5 transactions - all transactions
 const latestTransactions = await pushChain.tx.get('*', {
-  limit: 20,
+  limit: 5,
 });
-
-console.log(latestTransactions);
+console.log('Fetched the latest 5 transactions:', latestTransactions);
 
 // Fetch a transaction by category
 const transactionsByCategory = await pushChain.tx.get('*', {
   category: 'CUSTOM:SAMPLE_TX',
 });
-
-console.log(transactionsByCategory);
+console.log(
+  "Fetched transactions by category 'CUSTOM:SAMPLE_TX':",
+  transactionsByCategory
+);
 
 // Fetch transactions sent by a specific account
 const senderAccount = {
@@ -35,8 +36,11 @@ const senderAccount = {
 const transactionsBySender = await pushChain.tx.get(senderAccount, {
   filterMode: 'sender',
 });
-
-console.log(transactionsBySender);
+console.log(
+  'Fetched transactions sent by account:',
+  senderAccount.address,
+  transactionsBySender
+);
 
 // Fetch transactions received by a specific account
 const recipientAccount = {
@@ -48,12 +52,18 @@ const recipientAccount = {
 const transactionsByRecipient = await pushChain.tx.get(recipientAccount, {
   filterMode: 'recipient',
 });
-
-console.log(transactionsByRecipient);
+console.log(
+  'Fetched transactions received by account:',
+  recipientAccount.address,
+  transactionsByRecipient
+);
 
 // Fetch transactions sent *OR* received by a specific account
 const transactionsByAccount = await pushChain.tx.get(senderAccount, {
   filterMode: 'both',
 });
-
-console.log(transactionsByAccount);
+console.log(
+  'Fetched transactions sent or received by account:',
+  senderAccount.address,
+  transactionsByAccount
+);
