5e91e486bf290017136ce1577566ec80e4fd0288
aman035 <guptaaman200115@gmail.com>
2025-07-02 11:45:34 +0530

    chore(repo): update commitlint config

--- commitlint.config.js
+++ commitlint.config.js
@@ -16,6 +16,17 @@ module.exports = {
         'build',
       ],
     ],
+    'scope-enum': [
+      2,
+      'always',
+      [
+        // list all packages here
+        'core',
+        'ui-kit',
+        // list all monorepo-level scopes here
+        'repo',
+      ],
+    ],
     'scope-empty': [2, 'never'],
     'subject-empty': [2, 'never'],
   },
