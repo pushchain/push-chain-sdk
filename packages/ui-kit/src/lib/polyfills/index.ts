f858e433ed3694fd3a8ead82bec858b8ee545374
Riyanshu Patro <riyanshu@push.org>
2025-07-09 03:20:56 +0530

    remove buffer fix

--- packages/ui-kit/src/lib/polyfills/index.ts
+++ packages/ui-kit/src/lib/polyfills/index.ts
@@ -8,6 +8,6 @@ if (typeof globalThis.global === 'undefined') {
   (globalThis as any).global = globalThis;
 }
 
-if (typeof globalThis.Buffer === 'undefined') {
-  (globalThis as any).Buffer = NodeBuffer;
-}
+// if (typeof globalThis.Buffer === 'undefined') {
+//   (globalThis as any).Buffer = NodeBuffer;
+// }
